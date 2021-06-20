4bf4c382e8fa467aa5c5b3d885a6517bca535cea
RA <numpde@null.net>
2021-06-20 22:36:31 +0200

    add `Memo`

--- code/helpers/opt_utils/misc.py
+++ code/helpers/opt_utils/misc.py
@@ -1,13 +1,16 @@
 # RA, 2021-06-11
+# RA, 2021-06-20
 
-import os
 import json
-import inspect
 import datetime
 import pathlib
 import time
+import inspect
+import sorcery
+import executing
 
 REPO = next(p for p in pathlib.Path(__file__).parents for p in p.glob("code")).parent.resolve()
+
 
 class Section:
     def __init__(self, desc, out=print):
@@ -32,3 +35,29 @@ class JSONEncoder(json.JSONEncoder):
             return str(obj.resolve().relative_to(REPO))
         else:
             return json.JSONEncoder.default(self, obj)
+
+
+class Memo(dict):
+    """
+    Example:
+        memo = Memo()
+        (name, surname) = memo("X", "Y")
+        print(memo)  # {'name': 'X', 'surname': 'Y'}
+
+    Modified from `sorcery.spell` and `sorcery.assigned_names`.
+    """
+
+    def __call__(self, value0, *values):
+        exe = executing.Source.executing(inspect.currentframe().f_back)
+        names = sorcery.core.FrameInfo(exe).assigned_names(allow_one=True)[0]
+
+        assert len(names) == (1 + len(values))
+
+        for (name, value) in zip(names, (value0, *values)):
+            self[name] = value
+
+        if values:
+            return (value0, *values)
+        else:
+            return value0
+
