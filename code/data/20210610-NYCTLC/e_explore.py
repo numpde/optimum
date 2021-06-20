6316658330619a6dbebca921550a4180a543d804
RA <numpde@null.net>
2021-06-20 23:20:56 +0200

    add trip_speeds_times_square, trip_duration_vs_distance2

--- code/data/20210610-NYCTLC/e_explore.py
+++ code/data/20210610-NYCTLC/e_explore.py
@@ -6,8 +6,12 @@ https://github.com/numpde/transport/blob/master/pt2pt/20191021-NYCTLC/data_prepa
 """
 
 import calendar
+import contextlib
+import json
 from collections import Counter
+from datetime import timedelta
 
+from scipy.stats import gaussian_kde
 from twig import log
 
 from pathlib import Path
@@ -18,20 +22,27 @@ import pandas as pd
 
 from itertools import groupby
 
+from geopy.distance import distance as geodistance
+from tqdm import tqdm as progress
+import percache
+
 from plox import Plox, rcParam
 from tcga.utils import unlist1, mkdir, whatsmyname
 
 from opt_maps import maps
+from opt_utils.misc import Section
 from opt_utils.style import default_style, name2color, name2cmap
 
 BASE = Path(__file__).parent.resolve()
 out_dir = Path(__file__).with_suffix('')
 
+cache = percache.Cache(str(mkdir(Path(__file__).with_suffix('')) / "percache"), livesync=True)
+
 
 def QUERY(sql) -> pd.DataFrame:
     log.debug(f"Query: {sql}.")
     with connect(unlist1(BASE.glob("**/trips.db"))) as con:
-        return pd.read_sql_query(sql, con)
+        return pd.read_sql_query(sql, con, parse_dates=['ta', 'tb'])
 
 
 def trip_distance_histogram(table_name):
@@ -119,18 +130,100 @@ def pickup_hour_heatmap(table_name):
         px.f.savefig(mkdir(out_dir / f"{whatsmyname()}") / f"{table_name}.png")
 
 
+def trip_speeds_times_square(table_name):
+    @cache
+    def zoom(table_name: str, weekday: int, limit, radius, focus=(40.75798, -73.98550)):
+        sql = f"""
+            SELECT [ta], [tb], [xa_lat], [xa_lon], [xb_lat], [xb_lon], [distance] as ds, strftime('%w', ta) as weekday
+            FROM [{table_name}]
+            WHERE (weekday == '{weekday}')
+            ORDER BY RANDOM() 
+            LIMIT {limit}
+        """
+
+        # Note: could do `where` by lat/lon bbox
+
+        trips = QUERY(sql)
+
+        trips = trips.assign(xa=list(zip(trips.xa_lat, trips.xa_lon)))
+        trips = trips.assign(xb=list(zip(trips.xb_lat, trips.xb_lon)))
+
+        dist_to_focus = pd.Series(index=trips.index, data=[
+            max(geodistance(row.xa, focus).m, geodistance(row.xb, focus).m)
+            for (i, row) in progress(trips.iterrows(), total=len(trips))
+        ])
+
+        return trips[dist_to_focus <= radius]
+
+    query_params = dict(weekday=3, limit=1000000, radius=1000)
+
+    trips = zoom(table_name, **query_params)
+
+    MAX_TRIP_DURATION_Min = 30
+    trips = trips.assign(s=(trips.tb - trips.ta).dt.total_seconds())
+    trips = trips[trips.s <= timedelta(minutes=MAX_TRIP_DURATION_Min).total_seconds()]
+
+    trips = trips.assign(v=(trips.ds / trips.s))
+    (MIN_TRIP_SPEED, MAX_TRIP_SPEED) = (0.1, 15.5)
+    trips = trips[(MIN_TRIP_SPEED <= trips.v) & (trips.v <= MAX_TRIP_SPEED)]  # reasonable average speeds
+
+    log.info(f"Got {len(trips)} trips about Times Square from {table_name}.")
+
+    with (mkdir(out_dir / f"{whatsmyname()}") / f"{table_name}.json").open(mode='w') as fd:
+        with contextlib.redirect_stdout(fd):
+            print(json.dumps(indent=2, obj={
+                'query': query_params, 'trips': len(trips),
+                'min_trip_speed': MIN_TRIP_SPEED, 'max_trip_speed': MAX_TRIP_SPEED,
+                'max_trip_duration_minutes': MAX_TRIP_DURATION_Min,
+            }))
+
+    if not len(trips):
+        return
+
+    trips = trips.assign(h=trips.ta.dt.hour)  # hour of the day
+
+    from matplotlib.colors import LinearSegmentedColormap as LinearColormap
+    from matplotlib.ticker import MaxNLocator
+
+    cmap = LinearColormap.from_list(name="sun", colors=["black", "lightblue", "yellow", "orange", "black"])
+
+    with Plox({**default_style, rcParam.Figure.figsize: (8, 3)}) as px:
+        for (h, df) in trips.groupby(by='h'):
+            kde = gaussian_kde(df.v)
+            xx = np.linspace(0, MAX_TRIP_SPEED, 101)
+            yy = kde(xx)
+
+            more = dict(label=f"{h}h") if (h % 2) else {}
+            px.a.plot(xx, yy, lw=1, c=cmap([h / 24]), alpha=0.7, **more)
+            # px.a.hist(df.v, bins='fd', lw=2, density=True, histtype='step', color=cmap([h / 24]), zorder=10, alpha=0.7, label=f"{h}h")
+
+        xticks = list(range(int(np.ceil(MAX_TRIP_SPEED))))
+        px.a.set_xlim(0, MAX_TRIP_SPEED)
+        px.a.set_xticks(xticks)
+
+        # px.a.set_xscale('log')
+
+        px.a.set_yticks([])
+        px.a.grid(lw=0.3)
+
+        px.a.set_xticklabels(xticks)
+        px.a.set_xlabel("m/s")
+
+        [h.set_alpha(1) for h in px.a.legend(fontsize=9, loc='upper right').legendHandles]
+
+        px.f.savefig(mkdir(out_dir / f"{whatsmyname()}") / f"{table_name}.png")
+
+
 def trip_speed_histogram(table_name):
     import matplotlib.pyplot as plt
 
     sql = f"SELECT [ta] as ta, [tb] as tb, [distance] as m FROM [{table_name}]"
     # sql += "ORDER BY RANDOM() LIMIT 1000"  # DEBUG
     df = QUERY(sql)
-    df.ta = pd.to_datetime(df.ta)
-    df.tb = pd.to_datetime(df.tb)
     # Duration in seconds
     df['s'] = (df.tb - df.ta).dt.total_seconds()
     # Hour of the day
-    df['h'] = df.tb.dt.hour
+    df['h'] = df.tb.dt.hour  # why tb?
     # Forget pickup/dropoff times
     df = df.drop(columns=['ta', 'tb'])
     # Columns: distance-meters, duration-seconds, hour-of-the-day
@@ -143,12 +236,12 @@ def trip_speed_histogram(table_name):
 
     # Omit low- and high-speed trips [m/s]
     MIN_TRIP_SPEED = 0.1  # m/s
-    MAX_TRIP_SPEED = 15.5  # m/s
+    MAX_TRIP_SPEED = 15.5  # m/s  (do .5 for plotting)
     df = df[df.v >= MIN_TRIP_SPEED]
     df = df[df.v <= MAX_TRIP_SPEED]
 
     # Omit long-duration trips
-    MAX_TRIP_DURATION_H = 2  # hours
+    MAX_TRIP_DURATION_H = 1  # hours
     df = df[df.s < MAX_TRIP_DURATION_H * (Sec / H)]
 
     with plt.style.context({**Plox._default_style, **default_style}):
@@ -243,9 +336,63 @@ def trip_duration_vs_distance(table_name):
         px.f.savefig(mkdir(out_dir / f"{whatsmyname()}") / f"{table_name}.png")
 
 
+def trip_duration_vs_distance2(table_name):
+    N = 100000
+
+    sql = f"""
+        SELECT [ta], [tb], [distance] as ds
+        FROM [{table_name}]
+        ORDER BY RANDOM() 
+        LIMIT {N}
+    """
+
+    # WHERE ('2016-05-01 00:00' <= ta) and (tb <= '2016-05-02 08:30')
+
+    trips = QUERY(sql)
+
+    trips['dt'] = trips.tb - trips.ta
+
+    # Hour of the day
+    trips['h'] = trips['ta'].dt.hour
+
+    from matplotlib.colors import LinearSegmentedColormap as LinearColormap
+    from matplotlib.ticker import MaxNLocator
+
+    with Plox(default_style) as px:
+        px.a.grid()
+        # px.a.plot(*(2 * [[0, df[['reported', 'shortest']].values.max()]]), c='k', ls='--', lw=0.3, zorder=100)
+
+        for (h, df) in trips.groupby(by='h'):
+            trip_dist = df['ds'] / 1e3
+            trip_time = df['dt'].dt.total_seconds() / 60
+
+            cmap = LinearColormap.from_list(name="sun", colors=["black", "lightblue", "yellow", "orange", "black"])
+            px.a.scatter(
+                trip_time, trip_dist,
+                c=cmap([h / 24]), s=3, alpha=0.2, lw=0, zorder=-10,
+                label=f"{len(df)} trips at {h}h"
+            )
+
+        # px.a.scatter(traveldist, traveltime, alpha=0.01, edgecolors='none', c=name2color(table_name), zorder=N)
+
+        px.a.set_xlabel("Time, min")
+        px.a.set_ylabel("Distance, km")
+        px.a.set_xlim(0, 33)
+        px.a.set_ylim(0, 13)
+        px.a.grid(True, zorder=0, lw=0.3)
+        [h.set_alpha(1) for h in px.a.legend(fontsize=6, loc='upper left').legendHandles]
+
+        px.a.xaxis.set_major_locator(MaxNLocator(integer=True))
+        px.a.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+        px.f.savefig(mkdir(out_dir / f"{whatsmyname()}") / f"{table_name}.png")
+
+
 if __name__ == '__main__':
     ff = [
-        trip_distance_histogram,
+        trip_speeds_times_square,
+        # trip_duration_vs_distance2,
+        # trip_distance_histogram,
         # trip_trajectories_initial,
         # pickup_hour_heatmap,
         # trip_speed_histogram,
@@ -257,5 +404,5 @@ if __name__ == '__main__':
 
     for f in ff:
         for table_name in sorted(tables):
-            log.info(f"{f.__name__}({table_name})")
-            f(table_name)
+            with Section(f"Processing {f.__name__}({table_name})", out=log.info):
+                f(table_name)
