67a1a09f9f998098c139ae4a486f5b597a2dd8cc
RA <numpde@null.net>
2021-06-22 00:38:25 +0200

    helpers -- minor

--- code/helpers/opt_utils/misc.py
+++ code/helpers/opt_utils/misc.py
@@ -61,3 +61,56 @@ class Memo(dict):
         else:
             return value0
 
+
+def datatable_html(table_html: str):
+    # https://datatables.net/extensions/fixedheader/examples/options/columnFiltering.html
+    template = r"""
+    <html>
+    <head><title></title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/fixedheader/3.1.9/js/dataTables.fixedHeader.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.9/css/fixedHeader.dataTables.min.css">
+    <style>
+    thead input {{ width: 50px; }}
+    </style>
+    </head><body>
+    <p>
+    For an exact-match column search use regex, e.g. <b>^100$</b> or <b>10|20</b>
+    </p>
+
+    {table}
+
+    <script type="text/javascript">
+    $(document).ready(function() {
+        // Setup - add a text input to each footer cell
+        $('#data thead tr').clone(true).appendTo( '#data thead' );
+        $('#data thead tr:eq(1) th').each( function (i) {
+            var title = $(this).text();
+            $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+
+            $('input', this).on('keyup change', function () {
+                if (table.column(i).search() !== this.value) {
+                    // https://stackoverflow.com/questions/44003585/datatables-column-search-for-exact-match
+                    table.column(i).search(this.value, true, false).draw();
+                }
+            });
+
+            $('#data > thead > tr > th').css({'width': '100px'});
+            $('#data input').css({'width': '100%'});
+            $('#data').css({'width': '100%'});
+        } );
+
+        var table = $('#data').DataTable({
+            orderCellsTop: true,
+            fixedHeader: true,
+            "lengthMenu": [[-1, 8, 16, 32, 64], ["All", 8, 16, 32, 64]],
+            "pageLength": 16,
+        });
+    });
+    </script>
+    </body></html>
+    """
+
+    return template.replace("{table}", table_html)
